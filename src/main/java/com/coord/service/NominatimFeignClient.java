5b344204bb46038505b14fb51e6e356575312ed7
unamura <manuel.murari@gmail.com>
2021-10-14 18:08:15 +0200

    Feign client prima prova

--- src/main/java/com/coord/service/NominatimFeignClient.java
+++ src/main/java/com/coord/service/NominatimFeignClient.java
@@ -1,8 +1,15 @@
 package com.coord.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.coord.dto.GeoCode;
 
 @FeignClient(value = "openatim", url = "${feign.nominatim.url}")
-public class NominatimFeignClient {
+public interface NominatimFeignClient {
+
+	@RequestMapping("/search")
+	public GeoCode retrieveGeoCode(@RequestParam String format, @RequestParam String city);
 
 }
--- src/main/java/com/coord/service/TrialController.java
+++ src/main/java/com/coord/service/TrialController.java
@@ -1,5 +1,6 @@
 package com.coord.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class TrialController {
+	
+	@Autowired
+	private NominatimFeignClient clientFeign;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome() {
@@ -20,12 +24,19 @@ public class TrialController {
 	}
 
 	@RequestMapping("/geo/{city}")
-	public GeoCode getFromGoogle(@PathVariable(required = true) String city) {
+	public GeoCode getFromNominatim(@PathVariable(required = true) String city) {
 		log.info("Initializing getFromGoogle method");
 		String url = "https://nominatim.openstreetmap.org/search?format=geocodejson&city=" + city;
 		RestTemplate rt = new RestTemplate();
 		GeoCode gc = rt.getForObject(url, GeoCode.class);
 		
+		return gc;
+	}
+	
+	@RequestMapping("/geo")
+	public GeoCode getFromNominatimFeign() {
+		
+		GeoCode gc = clientFeign.retrieveGeoCode("geocodejson", "milan");
 		return gc;
 	}
 
